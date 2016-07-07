56768e006d5719dd32312b9174119525b2987c49
CuriosityEngineer <aeshta@gmail.com>
2016-07-08 00:19:03 +0200

    test controller index

--- spec/controllers/todos_controller_spec.rb
+++ spec/controllers/todos_controller_spec.rb
@@ -1,16 +1,19 @@
-# require 'rails_helper'
-#
-# RSpec.describe TodosController, type: :controller do
-#
-#   let!(:a_todo) { Todo.create! valid_attributes }
-#
-#   describe "GET #index" do
-#
-#     it "assigns all todos as @todos" do
-#       get :index
-#       expect(assigns(:todos)).to eq([a_todo])
-#     end
-#
-#   end
-#
-# end
+require 'rails_helper'
+
+describe TodosController do
+
+  describe "GET #index" do
+    it "gets index" do
+      get :index
+      expect(response).to be_success
+      expect(response).to render_template("index")
+    end
+
+    it "Assigns todos to @todos" do
+      todo = Todo.create(item: 'Wake up!')
+      get :index
+      expect(assigns[:todos]).to eq ([todo])
+    end
+  end
+
+end
