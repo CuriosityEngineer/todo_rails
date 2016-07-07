3fe0b299d3f7e9423550d9eeac9e0088f7421cb5
CuriosityEngineer <aeshta@gmail.com>
2016-07-08 01:13:17 +0200

    6 succes test controller

--- spec/controllers/todos_controller_spec.rb
+++ spec/controllers/todos_controller_spec.rb
@@ -16,4 +16,75 @@ describe TodosController do
     end
   end
 
+  # describe TodosController do
+  #   describe "GET 'index' " do
+  #     it "returns successful" do
+  #        get :index, format: :json
+  #       expect(response).to be_success
+  #     end
+  #   end
+  # end
+
+  describe "POST #create" do
+    context "with valid params" do
+      let(:valid_params) { { todo: { item: 'Hey!' } } }
+
+      it "creates a new Item" do
+        expect {
+          post :create, valid_params
+        }.to change(Todo, :count).by(1)
+      end
+
+      it "assigns a newly created todo as @todo" do
+        post :create, valid_params
+        expect(assigns(:todo)).to be_a(Todo)
+        expect(assigns(:todo)).to be_persisted
+      end
+    end
+  end
+
+  describe "PUT #update" do
+    let(:current_attributes) { { item: "Bla"} }
+
+    context "with valid params" do
+      let(:valid_params) { { item: "Hehehe" } }
+
+      it "updates the requested todo" do
+        todo = Todo.create! current_attributes
+        put :update, {:id => todo.to_param, :todo => valid_params}
+        todo.reload
+
+        expect(todo.item).to eq("Hehehe")
+      end
+
+      it "assigns the requested todo as @todo" do
+        todo = Todo.create! current_attributes
+        put :update, {:id => todo.to_param, :todo => valid_params}
+        expect(assigns(:todo)).to eq(todo)
+      end
+
+      # it "redirects to the todo" do
+      #   todo = todo.create! current_attributes
+      #   put :update, {:id => todo.to_param, :todo => valid_params}
+      #   expect(response).to redirect_to(todo)
+      # end
+    end
+
+  #   context "with invalid params" do
+  #     let(:invalid_params) { { item: nil} }
+  #
+  #     it "assigns the todo as @todo" do
+  #       todo = todo.create! current_attributes
+  #       put :update, {:id => todo.to_param, :todo => invalid_params}
+  #       expect(assigns(:todo)).to eq(todo)
+  #     end
+  #
+  #     it "re-renders the 'edit' template" do
+  #       todo = todo.create! current_attributes
+  #       put :update, {:id => todo.to_param, :todo => invalid_params}
+  #       expect(response).to render_template("edit")
+  #     end
+  #   end
+  end
+
 end
